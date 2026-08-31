15cc28aff39cabbdf41ab100096d44fe0395a00a
raghu <raghumsmp@gmail.com>
2026-08-31 17:55:59 +0530

    python introduction

---
+++ example.py
@@ -0,0 +1,36 @@
+print("hello, world!")
+print(5/5)
+
+import sys
+print(sys.version)
+
+if 5>2:
+
+ print("is greater ")
+else:
+ print("smaller")
+
+#variables
+x = 3
+y=("raju")
+print(x)
+print(y)
+print(x,y)
+
+x = str(3.2)
+y = int(3.4)
+z = float(3.6)
+
+print(x)
+print(y)
+print(z)
+
+x=6
+y=("raju")
+print(type(x))
+print(type(y))
+
+a=5
+A="python"
+print(a)
+print(A)
